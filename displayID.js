782418041af0eb60c4661e2f9f5b07cc1ab0927c
legopowa <legopowa@hotmail.com>
2023-09-04 21:02:35 -0700

    Update displayID.js

--- displayID.js
+++ displayID.js
@@ -24,7 +24,7 @@ window.onload = function() {
 }
 
 function verifyWithSteam() {
-    alert("Verify with Steam button clicked. test 7");
+    alert("Verify with Steam button clicked. test 8");
     const url = new URL(window.location.href);
     
     //alert("Sending verification request to Flask server...");
