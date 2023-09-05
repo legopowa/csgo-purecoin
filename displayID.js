524eb342910c9a8600980c10d30f7205311cc2b7
legopowa <legopowa@hotmail.com>
2023-09-04 20:13:39 -0700

    trying to verify captured openid token

--- displayID.js
+++ displayID.js
@@ -1,5 +1,3 @@
-// displayID.js
-
 window.onload = function() {
     const url = new URL(window.location.href);
     const openidIdentity = url.searchParams.get("openid.identity");
@@ -7,20 +5,45 @@ window.onload = function() {
     const openidSig = url.searchParams.get("openid.sig");
 
     if (openidIdentity) {
-        document.getElementById("steamIDDisplay").innerText = "Your Steam ID: " + openidIdentity;
+        document.getElementById("steamIDDisplay").innerText = openidIdentity;
     } else {
         document.getElementById("steamIDDisplay").innerText = "Failed to retrieve Steam ID.";
     }
 
     if (openidSigned) {
-        document.getElementById("signedFields").innerText = "Signed fields: " + openidSigned;
+        document.getElementById("signedFields").innerText = openidSigned;
     } else {
         document.getElementById("signedFields").innerText = "Failed to retrieve signed fields.";
     }
 
     if (openidSig) {
-        document.getElementById("signature").innerText = "Signature: " + openidSig;
+        document.getElementById("signature").innerText = openidSig;
     } else {
         document.getElementById("signature").innerText = "Failed to retrieve signature.";
     }
 }
+
+function verifyWithSteam() {
+    const url = new URL(window.location.href);
+    
+    fetch('https://steamcommunity.com/openid/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+            ...Array.from(url.searchParams.entries()).reduce((obj, [key, value]) => ({ ...obj, [key]: value }), {}),
+            'openid.ns': 'http://specs.openid.net/auth/2.0',
+            'openid.mode': 'check_authentication'
+        })
+    })
+    .then(response => response.text())
+    .then(data => {
+        if (data.includes('is_valid:true')) {
+            document.getElementById('verificationResult').innerText = 'OpenID response is valid!';
+        } else {
+            document.getElementById('verificationResult').innerText = 'OpenID response is NOT valid!';
+        }
+    });
+}
+
