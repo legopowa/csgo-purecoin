035f38124ee743a1d041e13ad2b8e771a22ccac0
legopowa <legopowa@hotmail.com>
2023-09-04 19:55:19 -0700

    capturing openid token

--- displayID.js
+++ displayID.js
@@ -1,9 +1,16 @@
 window.onload = function() {
     const url = new URL(window.location.href);
     const openidIdentity = url.searchParams.get("openid.identity");
+    const openidSigned = url.searchParams.get("openid.signed");
+    const openidSig = url.searchParams.get("openid.sig");
 
-    if (openidIdentity) {
+    if (openidIdentity && openidSigned && openidSig) {
         document.getElementById("steamIDDisplay").innerText = "Your Steam ID: " + openidIdentity;
+
+        // Store or display the signature and signed fields as needed
+        // For example, you could send these to your DApp validators for verification
+        console.log("Signed fields:", openidSigned);
+        console.log("Signature:", openidSig);
     } else {
         document.getElementById("steamIDDisplay").innerText = "Failed to retrieve Steam ID.";
     }
