5b3499c6f08f5a85d82ba53209f158ae05511eaf
legopowa <legopowa@hotmail.com>
2023-09-04 19:50:24 -0700

    typo fix

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function loginWithSteam() {
     const steamOpenIDURL = "https://steamcommunity.com/openid/login?" + 
         "openid.ns=http://specs.openid.net/auth/2.0&" +
         "openid.mode=checkid_setup&" +
-        "openid.return_to=https://legopowa.github.io/csgo-purecoin/GotSteamID.html" +
+        "openid.return_to=https://legopowa.github.io/csgo-purecoin/GotSteamID.html&" +
         "openid.realm=https://legopowa.github.io/csgo-purecoin/&" +  // Typically the base URL of your site
         "openid.identity=http://specs.openid.net/auth/2.0/identifier_select&" +
         "openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select";
